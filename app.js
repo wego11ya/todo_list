bb77246f4d102d589ff9dd098cf6caf323ff8ca8
wego11ya <wego11ya@gmail.com>
2022-09-29 17:48:10 +0800

    feat: modify index.hbs

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
+const Todo = require("./models/todo");
 const app = express();
 
 //設定連線到mongodb
@@ -20,7 +21,11 @@ app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  // 拿到全部的 Todo 資料
+  Todo.find()
+    .lean()
+    .then((todos) => res.render("index", { todos: todos }))
+    .catch((error) => console.error(error));
 });
 
 app.listen(3000, () => {
