ee8862ccdcc31900647ef41f953db0647cc023e1
wego11ya <wego11ya@gmail.com>
2022-09-29 16:11:06 +0800

    feat: add todo seeder

---
+++ models/seeds/todoSeeder.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Todo = require("../todo");
+
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+db.once("open", () => {
+  console.log("mongodb connected!");
+
+  for (let i = 0; i < 10; i++) {
+    Todo.create({ name: `name-${i}` });
+  }
+
+  console.log("done");
+});
