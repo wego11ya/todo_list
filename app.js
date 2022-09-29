e2bb38896a2d1dcbb55fd69c8034b1c63fa465aa
wego11ya <wego11ya@gmail.com>
2022-09-29 12:00:17 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+
+//設定連線到mongodb
+mongoose.connect(process.env.MONGODB_URI);
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
 
 app.get("/", (req, res) => {
   res.send("hello world!");
