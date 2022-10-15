285ca78faf15406e3f25b40f5ff7b7fc5e6caa14
wego11ya <wego11ya@gmail.com>
2022-10-15 16:32:34 +0800

    refactor: modify express routes

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
-const Todo = require("./models/todo");
 const methodOverride = require("method-override");
+const Todo = require("./models/todo");
+const routes = require("./routes");
 const app = express();
 
 //設定連線到mongodb
@@ -25,63 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  // 拿到全部的 Todo 資料
-  Todo.find()
-    .lean()
-    .sort({ _id: "asc" }) // 根據_id作升冪排序ascending
-    .then((todos) => res.render("index", { todos: todos }))
-    .catch((error) => console.error(error));
-});
-
-app.get("/todos/new", (req, res) => {
-  res.render("new");
-});
-
-app.post("/todos", (req, res) => {
-  const name = req.body.name;
-
-  return Todo.create({ name })
-    .then(() => res.redirect("/"))
-    .catch((error) => console.log(error));
-});
-
-app.get("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render("detail", { todo }))
-    .catch((error) => console.log(error));
-});
-
-app.get("/todos/:id/edit", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
-    .lean()
-    .then((todo) => res.render("edit", { todo }))
-    .catch((error) => console.log(error));
-});
-
-app.put("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  const { name, isDone } = req.body;
-  return Todo.findById(id)
-    .then((todo) => {
-      todo.name = name;
-      todo.isDone = isDone === "on";
-      return todo.save();
-    })
-    .then(() => res.redirect(`/todos/${id}`))
-    .catch((error) => console.log(error));
-});
-
-app.delete("/todos/:id", (req, res) => {
-  const id = req.params.id;
-  return Todo.findById(id)
-    .then((todo) => todo.remove())
-    .then(() => res.redirect("/"))
-    .catch((error) => console.log(error));
-});
+app.use(routes);
 
 app.listen(3000, () => {
   console.log("App is running on port http://localhost:3000");
---
+++ routes/modules/home.js
@@ -0,0 +1,15 @@
+// 引用EXPRESS及EXPRESS路由器
+const express = require("express");
+const router = express.Router();
+// 引用 Todo model
+const Todo = require("../../models/todo");
+// 定義首頁路由
+router.get("/", (req, res) => {
+  Todo.find()
+    .lean()
+    .sort({ _id: "asc" })
+    .then((todos) => res.render("index", { todos }))
+    .catch((error) => console.log(error));
+});
+// 匯出路由模組
+module.exports = router;
