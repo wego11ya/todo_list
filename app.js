31fb96c640a1ed8a6cbf167cb3b17ff2789cb7c2
wego11ya <wego11ya@gmail.com>
2022-10-16 13:37:27 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,23 +1,9 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const exphbs = require("express-handlebars");
 const methodOverride = require("method-override");
-const Todo = require("./models/todo");
 const routes = require("./routes");
+require("./config/mongoose");
 const app = express();
-
-//設定連線到mongodb
-mongoose.connect(process.env.MONGODB_URI);
-
-const db = mongoose.connection;
-
-db.on("error", () => {
-  console.log("mongodb error!");
-});
-
-db.once("open", () => {
-  console.log("mongodb connected!");
-});
 
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 app.set("view engine", "hbs");
--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -1,17 +1,7 @@
-const mongoose = require("mongoose");
 const Todo = require("../todo.js");
-
-mongoose.connect(process.env.MONGODB_URI);
-
-const db = mongoose.connection;
-
-db.on("error", () => {
-  console.log("mongodb error!");
-});
+const db = require("../../config/mongoose");
 
 db.once("open", () => {
-  console.log("mongodb connected!");
-
   for (let i = 0; i < 10; i++) {
     Todo.create({ name: `name-${i}` });
   }
